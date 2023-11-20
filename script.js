685533441aea53477962c0918a3b5faf4239bc51
mohamed hanou <mhanou442@gmail.com>
2023-11-20 08:34:24 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function changeimage(element,image){
+    element.src=image;
+}
+
+let elt = document.getElementById("12");
+setTimeout(changeimage(elt,'icons/maiilot vert.jpg'),2);
+
+
+
+/*card.onclick=function() {
+    card.img.src="icons/maiilot vert.jpg";
+}*/
